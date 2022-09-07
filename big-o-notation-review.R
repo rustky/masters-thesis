e0825b7314ed59bc39a554e623e180add49ca5c6
rustky <kylerustxc@gmail.com>
2022-09-07 11:25:21 -0700

    add Big-O notation review

---
+++ big-o-notation-review.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+library(data.table)
+library(ggrepel)
+
+x <- 1:10
+
+linear <- function(x){
+  x
+}
+
+log.linear <- function(x){
+  x * log(x)
+}
+
+quadratic <- function(x){
+  x^2
+}
+functions <- c("linear"=linear, "logarithmic"=log, "log-linear"=log.linear, "quadratic"=quadratic)
+big.O <- c("linear"="O(n)", "logarithmic"="O(log(n))", "log-linear"="O(nlog(n))", "quadratic"="O(n^2)")
+y.list <- list()
+for(i in names(functions)){
+  for(input in x){
+    temp = data.table()
+    print(functions[i])
+    temp$output <- lapply(input,functions[[paste(i)]])
+    temp$func <- i
+    temp$algorithm <- big.O[i]
+    y.list[[paste(i,input)]] <- temp
+  }
+}
+
+y <- do.call("rbind", y.list)
+y$input <- rep(1:10,4)
+y.label <- y[y[, .I[output == max(output)], by=func]$V1]
+ggplot(y)+
+  geom_line(aes(input, output, color=func))+
+  scale_x_continuous(breaks=c(1:10))+
+  geom_label(data = y.label, aes(x=input, y=output, label=algorithm),
+             nudge_x = -0.35,
+             nudge_y = 3)+
+  ggtitle("Big-O Notation Review")
+  
