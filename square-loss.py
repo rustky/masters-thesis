b327edf8e46e192d06c6335701d53d598736311f
rustky <kylerustxc@gmail.com>
2022-10-03 20:41:47 -0700

    update loss functions

--- square-loss.py
+++ square-loss.py
@@ -3,11 +3,12 @@ import torch
 def square_loss(predictions, labels, margin=1):
     I_pos = torch.where(labels == 1)[0]
     I_neg = torch.where(labels == 0)[0]
+    N = len(I_pos)*len(I_neg)
     z_coeff = margin - predictions[I_pos]
-    a_coeff = torch.sum(labels[I_pos])
-    b_coeff = torch.sum(2*z_coeff*labels[I_pos], dim = 0)
-    c_coeff = torch.sum((z_coeff**2)*labels[I_pos], dim = 0)
+    a_coeff = torch.sum((labels[I_pos])/N)
+    b_coeff = torch.sum((2*z_coeff*labels[I_pos])/N, dim = 0)
+    c_coeff = torch.sum(((z_coeff**2)*labels[I_pos])/N, dim = 0)
     predicted_value = predictions[I_neg]
     loss_values = a_coeff*(predicted_value**2) + b_coeff*predicted_value + c_coeff
-    running_loss = torch.sum(loss_values)/(len(I_pos)*len(I_neg))
+    running_loss = torch.sum(loss_values)
     return running_loss
--- squared-hinge-loss.py
+++ squared-hinge-loss.py
@@ -1,6 +1,6 @@
 import torch
 
-def square_hinge_loss(predictions, labels, margin=1):
+def squared_hinge_loss(predictions, labels, margin=1):
     torch.autograd.set_detect_anomaly(True)
     labels_length = len(labels)
     augmented_predictions = torch.zeros(labels_length)
@@ -14,9 +14,10 @@ def square_hinge_loss(predictions, labels, margin=1):
     labels_sorted = labels[augmented_predictions_sorted]
     I_pos = torch.where(labels_sorted == 1)[0]
     I_neg = torch.where(labels_sorted == 0)[0]
+    N = len(I_pos)*len(I_neg)
     z_coeff = margin - predicted_value
-    a_coeff = torch.cumsum(labels_sorted, dim = 0)
-    b_coeff = torch.cumsum(2*z_coeff*labels_sorted, dim = 0)
-    c_coeff = torch.cumsum((z_coeff**2)*labels_sorted, dim = 0)
+    a_coeff = torch.cumsum((labels_sorted)/N, dim = 0)
+    b_coeff = torch.cumsum((2*z_coeff*labels_sorted)/N, dim = 0)
+    c_coeff = torch.cumsum(((z_coeff**2)*labels_sorted)/N, dim = 0)
     loss_values = a_coeff*(predicted_value**2) + b_coeff*predicted_value + c_coeff
-    return sum(loss_values[I_neg])/(len(I_pos)*len(I_neg))
+    return sum(loss_values[I_neg])
