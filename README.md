ec393eea6d7f6fb57c292dc0f53fa2b4e80ef4cd
rustky <kylerustxc@gmail.com>
2022-10-24 13:38:51 -0700

    remake timing figure

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # masters-thesis
 
+## 10/24/2022
+[timing-experiment.ipynb](https://github.com/rustky/masters-thesis/blob/main/timing-experiment.ipynb) makes [timing-data.csv](https://github.com/rustky/masters-thesis/blob/main/timing-data.csv)
+[timing_figure.R](https://github.com/rustky/masters-thesis/blob/main/timing_figure.R) uses [timing-data.csv](https://github.com/rustky/masters-thesis/blob/main/timing-data.csv) to make [proof-of-concept-timing.png](https://github.com/rustky/masters-thesis/blob/main/proof-of-concept-timing.png)
 ## 9/8/2022
 [early-stopping-regularization.R](https://github.com/rustky/masters-thesis/blob/main/early-stopping-regularization.R) makes [early-stopping-regularization.png](https://github.com/rustky/masters-thesis/blob/main/early-stopping-regularization.png)
 
--- timing_figure.R
+++ timing_figure.R
@@ -4,12 +4,15 @@ library(ggplot2)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 timing.dt <- fread("timing_data.csv")
 
-subset.dt <- timing.dt[timing.dt[,.I[which.max(time_limit)],by=.(loss_name,
-                                                                data_size)]$V1]
+# subset.dt <- timing.dt[timing.dt[,.I[which.max(time_limit)],by=.(loss_name,
+#                                                                 data_size)]$V1]
 
-ggplot(subset.dt)+
+ggplot(timing.dt)+
   geom_line(aes(data_size, median_time,color=loss_name))+
-  geom_ribbon(aes(x=data_size, y=median_time, ymin=min_time, ymax=max_time,
+  geom_ribbon(aes(x=data_size, y=median_time, ymin=lower_quantile, ymax=upper_quantile,
                   fill=loss_name), alpha = 0.5)+
   scale_x_log10()+
-  scale_y_log10()
+  scale_y_log10()+
+  ylab('Time to compute the loss (s)')+
+  xlab('Number of Observations to Compute')+
+  ggtitle("Time To Compute The Loss With Enforced Time Limit")
