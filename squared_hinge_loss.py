a86596727dbdc77ad1cb43e0c989bb659aa48d22
rustky <kylerustxc@gmail.com>
2022-11-02 12:16:53 -0700

    clean up squared hinge loss

--- squared_hinge_loss.py
+++ squared_hinge_loss.py
@@ -1,17 +1,13 @@
-import torch
+import torch 
 
 def squared_hinge_loss(predictions, labels, margin=1):
     torch.autograd.set_detect_anomaly(True)
     labels_length = len(labels)
-    augmented_predictions = torch.zeros(labels_length)
-    for i in range(0, labels_length):
-        if labels[i] == 0:
-            augmented_predictions[i] = predictions[i] + margin
-        else:
-            augmented_predictions[i] = predictions[i]
-    augmented_predictions_sorted = torch.argsort(augmented_predictions)
-    predicted_value = predictions[augmented_predictions_sorted]
-    labels_sorted = labels[augmented_predictions_sorted]
+    augmented_predictions = torch.where(labels == 1, predictions,
+                                        predictions + margin)
+    augmented_indices_sorted = torch.argsort(augmented_predictions)
+    predicted_value = predictions[augmented_indices_sorted]
+    labels_sorted = labels[augmented_indices_sorted]
     I_pos = torch.where(labels_sorted == 1)[0]
     I_neg = torch.where(labels_sorted == 0)[0]
     N = len(I_pos)*len(I_neg)
