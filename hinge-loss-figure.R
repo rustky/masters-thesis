1861fa83edc0f4d6754c9cba7ec9852fb0f31e6e
rustky <kylerusxc@gmail.com>
2022-11-17 15:19:04 -0700

    tweak loss figure

--- hinge-loss-figure.R
+++ hinge-loss-figure.R
@@ -25,34 +25,35 @@ dt$y[0:12] <- 0
 points <- data.table(
   x = c(-5, -3, 1, 3, 5),
   y = c(0,0,0,0,0),
-  h = c("$s_{j_1}=1$", "$s_{j_2}=-1$","$s_{j_3}=1$",
-        "$s_{j_4}=-1$", "$s_{j_5}=1$")
+  h = c("$y_{s_1}=1$", "$y_{s_2}=-1$","$y_{s_3}=1$",
+        "$y_{s_4}=-1$", "$y_{s_5}=1$")
 )
 
 colnames(dt) <- c('H1', 'H3','H5', 'y')
 standAlone <- TRUE
 suffix <- if(standAlone)"-standAlone" else ""
-no.ext <- paste0("figure-fn-not-monotonic-error", suffix)
-f.tex <- paste0(no.ext, ".tex")
 tikz(file = '~/Documents/masters-thesis/hinge-loss.tex', standAlone = standAlone)
 plot <- ggplot(dt)+
     geom_line(aes(H1, y))+
     geom_line(aes(H3, y))+
     geom_line(aes(H5, y))+
     geom_point(data = points, aes(x, y))+
-    geom_text(data = points, aes(x,y, label = h, size = 30), nudge_y = 1)+
-    geom_segment(aes(x = -5, xend = -3,y = -0.75, yend = -0.75), size = 3, color = 'red')+
-    geom_segment(aes(x = 1, xend = 3,y = -1.5, yend = -1.5), size = 3, color = 'red')+
-    geom_segment(aes(x = -5, xend = 3,y = -2.25, yend = -2.25), size = 3, color = 'red')+
+    geom_text(data = points[c(1,3,5),], aes(x,y, label = h), size = 8,
+              nudge_y = -.5)+
+    geom_text(data = points[c(2,4),], aes(x,y, label = h), size = 8,
+              nudge_y = .5, nudge_x = .5)+
+    geom_segment(aes(x = -5, xend = -3,y = -1.25, yend = -1.25), linewidth = 3, color = 'red')+
+    geom_segment(aes(x = 1, xend = 3,y = -2, yend = -2), linewidth = 3, color = 'red')+
+    geom_segment(aes(x = -5, xend = 3,y = -2.75, yend = -2.75), linewidth = 3, color = 'red')+
     theme(text = element_text(size = 20),
           legend.position = 'none')+
     xlab("Augmented Predictions")+
     ylab("Loss")+
-    ggtitle("Sorted Predictions Acrue Loss")
+    ggtitle("Sorted Predictions Acrue Loss")+
+    ylim(-2.9, 9)
 print(plot)
 dev.off()
-tools::texi2dvi("~/Documents/masters-thesis/hinge-loss.tex",pdf=TRUE)
-tools::texi2dvi("~/Documents/masters-thesis/hinge-loss.tex",pdf=TRUE)
+tinytex::pdflatex("~/Documents/masters-thesis/hinge-loss.tex")
 # Error in plot.xy(xy.coords(x, y), type = type, ...) : 
   # plot.new has not been called yet
 
