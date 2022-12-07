e111fd1630c2b2d627bd724e95079aa9769e8fff
rustky <kylerustxc@gmail.com>
2022-12-06 17:55:56 -0700

    various figure update

--- bad-roc-curve.R
+++ bad-roc-curve.R
@@ -25,20 +25,20 @@ dt <- data.table(
 
 area_under_curve(dt$FPR, dt$TPR)
 
-ggplot(dt)+
-  geom_point(aes(FPR, TPR))+
-  geom_text(aes(FPR, TPR, label = paste("q=", q, sep = ' ')),nudge_y =  -0.02, nudge_x = 0.05)+
-  geom_text(aes(0.5 , 0.75, label = paste('AUC =', AUC, sep = ' ')), size = 9)+
-  geom_line(aes(FPR, TPR))+
-  geom_area(aes(FPR, TPR), alpha = 0.2)+
-  xlab("False Positive Rate")+
-  ylab("True Positive Rate")+
-  scale_x_continuous(breaks=c(0,0.5,1))+
-  scale_y_continuous(breaks=c(0,0.5,1))+
-  theme_bw()+
-  theme(panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        axis.line = element_line(colour = "black"),
-        text = element_text(size = 22, face = "bold"))
+plot <- ggplot(dt)+
+    geom_point(aes(FPR, TPR))+
+    geom_text(aes(FPR, TPR, label = paste("q=", q, sep = ' ')),nudge_y =  -0.02, nudge_x = 0.05)+
+    geom_text(aes(0.5 , 0.75, label = paste('AUC =', AUC, sep = ' ')), size = 9)+
+    geom_line(aes(FPR, TPR))+
+    geom_area(aes(FPR, TPR), alpha = 0.2)+
+    xlab("False Positive Rate")+
+    ylab("True Positive Rate")+
+    scale_x_continuous(breaks=c(0,0.5,1))+
+    scale_y_continuous(breaks=c(0,0.5,1))+
+    theme_bw()+
+    theme(panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          axis.line = element_line(colour = "black"),
+          text = element_text(size = 22, face = "bold"))
   
   
