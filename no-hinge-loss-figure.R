940cf8b29fbda97bb6b2c7b6ab055ae37c86d097
rustky <kylerustxc@gmail.com>
2022-12-07 17:58:59 -0700

    update no hinge figure code

--- no-hinge-loss-figure.R
+++ no-hinge-loss-figure.R
@@ -1,9 +1,7 @@
 library(data.table)
 library(tikzDevice)
 library(ggplot2)
-library(data.table)
-library(tikzDevice)
-library(ggplot2)
+library(directlabels)
 
 parabola <- function(x, b){
   if(b == 2){
@@ -21,11 +19,15 @@ parabola <- function(x, b){
 b_vals <- c(2,-4, -8)
 x <- seq(-3, 5, by = 0.25)
 
+label.lookup <- list("2"= "$a_1,b_1,c_1$",
+                     "4"=  "$a_2,b_2,c_2$", "8"=  "$a_3,b_3,c_3$")
+
 x.list <- list()
 for(b in b_vals){
   x.list[[paste("y",abs(b),sep = "-")]] <- parabola(x, b)
   x.list[[paste("y-color",abs(b),sep = "-")]] <- paste("y",abs(b),sep = "-s")
   x.list[[paste("y-set",abs(b),sep = "-")]] = "$\\mathcal{I^+}$"
+  x.list[[paste("y-label",abs(b),sep = "-")]] <- label.lookup[[paste(abs(b))]]
 }
 
 dt <- data.table(data.frame(x.list))
@@ -33,11 +35,14 @@ dt$y.total <- dt$y.2 + dt$y.4
 dt$y.color.total <- rep("y-total", nrow(dt))
 dt$predicted.value <- x
 dt$y.set.total <- '$\\mathcal{I^-}$'
+dt$y.label.total <- "$a^+,b^+,c^+$"
 
-dt.tall <- melt(dt, id.vars = c("predicted.value"), measure.vars = c("y.2", "y.4", "y.8", "y.total"),
+dt.tall <- melt(dt, id.vars = c("predicted.value"), 
+                measure.vars = c("y.2", "y.4", "y.8", "y.total"),
                 variable.name = "color", value.name = c("y"))
 set.dt <- melt(dt, measure.vars = c("y.set.2", "y.set.4", "y.set.8", "y.set.total"), variable.name = "name", value.name = "set")
-plot.dt <- data.table(dt.tall, set= set.dt$set)
+label.dt <- melt(dt, measure.vars = c("y.label.2", "y.label.4", "y.label.8", "y.label.total"), variable.name = "name", value.name = "label")
+plot.dt <- data.table(dt.tall, set= set.dt$set, label=label.dt$label)
 plot.dt$set <- factor(plot.dt$set, levels = c('$\\mathcal{I^+}$', '$\\mathcal{I^-}$'))
 
 # ggplot(plot.dt)+
@@ -59,7 +64,7 @@ tikz(file = '~/Documents/masters-thesis/no-hinge-loss.tex', standAlone = standAl
      width = 6, height = 4)
 
 plot <- ggplot(plot.dt)+
-  geom_line(aes(predicted.value, y, color= color ))+
+  geom_line(aes(predicted.value, y, color= color))+
   geom_segment(data = plot.dt[(predicted.value == 4)&(set == "$\\mathcal{I^-}$")],
                aes(x = predicted.value, xend = predicted.value, y = 0, yend = y),
                arrow=arrow(length = unit(0.1, "cm")))+
@@ -69,9 +74,12 @@ plot <- ggplot(plot.dt)+
   geom_segment(data = plot.dt[(x == -1)&(set == "$\\mathcal{I^-}$")],
                aes(x = predicted.value, xend = predicted.value, y = 0, yend = y),
                arrow=arrow(length = unit(0.1, "cm")))+
+  geom_dl(aes(predicted.value,y,label=label,color = color),
+          method = list("first.qp", cex = 1))+
   xlab("Predicted Value")+
   ggtitle("Square Loss Accrues Loss")+
   ylim(0,75)+
+  xlim(-5,5)+
   facet_grid(. ~ set)+
   theme(legend.position="none",
         text = element_text(size = 16))
